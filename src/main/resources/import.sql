7100caa774c30c446f5b695cf012b7a4fbd27e0c
Daniel <angelicodaniel93@gmail.com>
2023-06-29 13:41:05 +0200

    first commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,17 @@
+INSERT INTO pizzas (name, description, image, price) VALUES ('Margherita', 'Classic pizza with tomato sauce, mozzarella, and basil', 'margherita.jpg', 3.99);
+
+INSERT INTO pizzas (name, description, image, price) VALUES ('Diavola', 'Spicy pizza with tomato sauce, mozzarella, and spicy salami', 'diavola.jpg', 10.99);
+
+INSERT INTO pizzas (name, description, image, price) VALUES ('Vegetariana', 'Pizza loaded with assorted vegetables and cheese', 'vegetariana.jpg', 10.99);
+
+INSERT INTO pizzas (name, description, image, price) VALUES ('Quattro Formaggi', 'Pizza topped with four different types of cheese', 'quattro_formaggi.jpg', 12.99);
+
+INSERT INTO pizzas (name, description, image, price) VALUES ('Pizza al Pistacchio', 'Pizza with creamy pistachio sauce and mozzarella', 'pistacchio.jpg', 11.99);
+
+INSERT INTO pizzas (name, description, image, price) VALUES ('Capricciosa', 'Pizza with tomato sauce, mozzarella, ham, mushrooms, artichokes, olives, and oregano', 'capricciosa.jpg', 12.99);
+
+INSERT INTO pizzas (name, description, image, price) VALUES ('Bufalina', 'Pizza with tomato sauce, buffalo mozzarella, and fresh basil', 'bufalina.jpg', 11.99);
+
+INSERT INTO pizzas (name, description, image, price) VALUES ('Prosciutto e Funghi', 'Pizza with tomato sauce, mozzarella, ham, and mushrooms', 'prosciutto_funghi.jpg', 10.99);
+
+INSERT INTO pizzas (name, description, image, price) VALUES ('Pizza Melanzana', 'Pizza with yellow tomato sauce, donkey milk mozzarella, smoked herring, silphium and cumin', 'pizza_melanzana.jpg', 869.51);
